b4953fe03aa852464faed178c000f7a468ea460a
Alex <oles.mob1@gmail.com>
2021-04-15 02:41:14 +0300

    Finished hw5 and start work with hw6

--- hw5/Dog.java
+++ hw5/Dog.java
@@ -2,7 +2,7 @@ package crashCourseSoftServe.hw5;
 
 public class Dog {
 
-    enum breed{
+    enum Breed {
         COCKER,
         LABRADOR,
         BIGL,
@@ -11,11 +11,12 @@ public class Dog {
 
     private String name;
     private int age;
-    private String breed;
+    private Breed breed;
 
-    public Dog(String name, int age) {
+    public Dog(String name, int age, Breed breed) {
         this.name = name;
         this.age = age;
+        this.breed = breed;
     }
 
     public String getName() {
@@ -43,26 +44,26 @@ public class Dog {
 //        return name.compareTo(getName());
 //    }
 
-    Dog first = new Dog("num1",5);
-    Dog second = new Dog("num2",2);
-    Dog third = new Dog("num3",9);
+    Dog first = new Dog("num1", 5,Breed.HUSKY);
+    Dog second = new Dog("num2", 2,Breed.COCKER);
+    Dog third = new Dog("num3", 9, Breed.LABRADOR);
 
-    public void checkName(){
-        if(first.getName().equals(second.getName()) || second.getName().equals(third.getName()) || first.getName().equals(third.getName())){
+    public void checkName() {
+        if (first.getName().equals(second.getName()) || second.getName().equals(third.getName()) || first.getName().equals(third.getName())) {
             System.out.println("We have the same name");
-        }else{
+        } else {
             System.out.println("We don't have duplicate");
         }
     }
 
-    public void compareAge(){
-        if(first.getAge()<second.getAge()&&second.getAge()<third.getAge()){
-            System.out.println(third.name+" have the biggest age, it's "+third.getAge());
-        }else if (second.getAge()<third.getAge()&&third.getAge()<first.getAge()){
-            System.out.println(first.name+" have the biggest age, it's "+first.getAge());
-        }else if(third.getAge()<first.getAge()&&first.getAge()<second.getAge()){
-            System.out.println(second.name+" have the biggest age, it's "+second.getAge());
-        }else{
+    public void compareAge() {
+        if (first.getAge() < second.getAge() && second.getAge() < third.getAge()) {
+            System.out.println(third.name + " have the biggest age, it's " + third.getAge());
+        } else if (second.getAge() < third.getAge() && third.getAge() < first.getAge()) {
+            System.out.println(first.name + " have the biggest age, it's " + first.getAge());
+        } else if (third.getAge() < first.getAge() && first.getAge() < second.getAge()) {
+            System.out.println(second.name + " have the biggest age, it's " + second.getAge());
+        } else {
             System.out.println("All age have one number");
         }
     }
--- study/Test.java
+++ study/Test.java
@@ -1,17 +1,21 @@
 package crashCourseSoftServe.study;
 
-import java.util.*;
+public class Test{
 
-public class Test {
-    public static void main(String [] args) {
-        TreeSet<String> set=new TreeSet<>();
-        set.add("I");
-        set.add("would like to");
-        set.add("learn");
-        set.add("Java");
-        for (String s:set) {
-            System.out.println(s+" ");
-
+    public static void main(String s [] ){
+        try{
+            throw new CustomException("Custom exception");
+        }catch (CustomException e){
+            System.err.println(e.getMessage());
+        }finally {
+            System.out.println("the end");
         }
+    }
+
+}
+class CustomException extends Exception{
+    private static final long serialVersionUID = 1L;
+    public CustomException(String message){
+        super(message);
     }
 }
